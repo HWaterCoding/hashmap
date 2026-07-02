4870d36894d7f1a809d4e6211264b37613e9173f
HWaterCoding <183124787+HWaterCoding@users.noreply.github.com>
2026-07-01 20:21:02 -0400

    Install Jest and config files for testing purposes

--- hashmap.js
+++ hashmap.js
@@ -3,44 +3,56 @@ export default class HashMap{
     constructor(loadFactor, capacity){
         this.loadFactor = loadFactor;
         this.capacity = capacity;
+        //add this.buckets?
+        //add this.size?
     }
 
+    //takes a key and produces a hashcode
     hash(key){
 
     }
 
+    //takes an argument of a key and value and assigns the value to the key
     set(key, value){
 
     }
 
+    //takes a key as an argument and returns the value 
     get(key){
 
     }
 
+    //returns true or false based on whether the key passed in exists within the hashmap
     has(key){
 
     }
 
+    // takes a key as an argument to remove 
     remove(key){
 
     }
 
+    // returns the total number of keys stored in the hashmap
     length(){
 
     }
 
+    // removes ALL entries stores in the hashmap
     clear(){
 
     }
 
+    // returns an array containing every key contained within the hashmap
     keys(){
 
     }
 
+    // returns an array containing every value contained within the hashmap
     values(){
 
     }
 
+    // returns an array containing every key:value pair contained within the hashmap
     entries(){
 
     }
