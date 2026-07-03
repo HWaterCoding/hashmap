82609e7faab6e22e53bc3339ca6d96740db09d61
HWaterCoding <183124787+HWaterCoding@users.noreply.github.com>
2026-07-02 20:14:54 -0400

    Write first 3 Jest tests to test hashing function Pass all 3 Jest tests

--- hash.test.js
+++ hash.test.js
@@ -1,3 +1,18 @@
 //testing file
 import HashMap from "./hashmap.js";
 
+//hashing function
+test("hashes a single character", ()=>{
+    const hashmap = new HashMap();
+    expect(hashmap.hash("a")).toBe(1);
+});
+
+test("hashes a string", ()=>{
+    const hashmap = new HashMap();
+    expect(hashmap.hash("Hello")).toBe(2);
+});
+
+test("different output for a string with the same characters", ()=>{
+    const hashmap = new HashMap();
+    expect(hashmap.hash("olHes")).toBe(9);
+});
